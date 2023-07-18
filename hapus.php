462713f1d5ff41f0a99d73ce823383b8f80c240b
Nahayaa Akbar Fahrialsyah <134659393+Pepepe23@users.noreply.github.com>
2023-07-18 07:48:29 +0700

    Add files via upload

---
+++ hapus.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'koneksi.php';
+
+$hapus = "DELETE FROM tamu WHERE id = ?";
+$hapus1 = "DELETE FROM kamar WHERE id = ?";
+
+$stmt = $koneksi->prepare($hapus);
+$stmt1 = $koneksi->prepare($hapus1);
+
+$stmt->bind_param("s", $_GET['id']);
+$stmt1->bind_param("s", $_GET['id']);
+
+if ($stmt->execute() && $stmt1->execute()) {
+    echo "<script language=javascript>parent.location.href='rekap.php';</script>";
+} else {
+    echo "Gagal menghapus data";
+}
+
+$stmt->close();
+$stmt1->close();
+$koneksi->close();
+?>
